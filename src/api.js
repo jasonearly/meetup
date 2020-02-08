6421c7ed2df041b33d425e1d968b9c62206463c4
Jason Early <theoffice@gruntmonkey.com>
2020-02-08 13:58:26 -0600

    removed offline text

--- src/api.js
+++ src/api.js
@@ -45,10 +45,6 @@ async function getEvents(lat, lon, page) {
   }
 
   if (!navigator.onLine) {
-    this.setState({
-      infoText:
-        "You are currently offline. The displayed events might not be up to date. The search feature has also been disabled."
-    });
     const events = localStorage.getItem("lastEvents");
     return JSON.parse(events);
   }
